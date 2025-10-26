5e0ebd07a05433186717d5496578901dfb8daa15
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-10-26 15:13:28 +0300

    Ders14

---
+++ 14-Ders/kod.js
@@ -0,0 +1,18 @@
+console.log("Merhaba Dünya!");
+
+console.log("tugay tuna");
+
+var isim = "ahmet"; // string
+var soyisim = "tuna";
+
+var sayi1 = 12; // int
+var sayi2 = 2;
+
+console.log(isim, soyisim);
+
+// console.log(soyisim);
+
+console.log(sayi1 + sayi2);
+console.log(sayi1 - sayi2);
+console.log(sayi1 * sayi2);
+console.log(sayi1 / sayi2);
